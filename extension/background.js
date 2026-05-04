34e60e7a616dee355602f8e2ff223ecf7909b1ea
Marc Jacob <mvnjacob@addu.edu.ph>
2026-05-04 14:44:34 +0800

    Added the browser popup

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,31 @@
+/**
+ * TruthLayer - Background Service Worker
+ * Handles communication between popup and backend API.
+ */
+const API_URL = 'http://localhost:3000/analyze';
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'ANALYZE_TEXT') {
-        fetch('http://localhost:3000/analyze', {
+    if (request.type === 'ANALYZE_CONTENT') {
+        fetch(API_URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ text: request.text })
+            body: JSON.stringify({
+                text: request.content,
+                url: request.url
+            })
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Server responded with ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => sendResponse({ success: true, data }))
         .catch(error => sendResponse({ success: false, error: error.message }));
-        
-        // Return true to indicate we will send a response asynchronously
-        return true; 
+
+        // Return true to indicate async response
+        return true;
     }
 });
